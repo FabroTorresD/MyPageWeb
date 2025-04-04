ecc30877e86cd58fec7b97bfb60a1227151df419
unknown <94231@sistemas.frc.utn.edu.ar>
2025-04-04 19:31:44 -0300

    daleee

--- MiPaginaWeb/mi-landing/vite.config.js
+++ MiPaginaWeb/mi-landing/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-landing/',
+  base: '/',  // Asegura que los archivos se sirvan desde la raíz
 });
